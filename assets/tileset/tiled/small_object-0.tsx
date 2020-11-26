5ac2ee156fa903756806570b9f39a69ab54992c8
kanatos-kun <geoffreydu89@gmail.com>
2020-11-26 13:36:42 +0100

    rm folder

--- assets/tileset/tiled/small_object-0.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="small_object-0" tilewidth="645" tileheight="441" tilecount="20" columns="5">
- <grid orientation="orthogonal" width="477" height="441"/>
- <image source="../small_object-0.png" width="3225" height="1764"/>
-</tileset>
--- assets/tileset/tiled/very_small_object-0.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="very_small_object-0" tilewidth="348" tileheight="281" tilecount="35" columns="5">
- <grid orientation="orthogonal" width="311" height="311"/>
- <image source="../very_small_object-0.png" width="1740" height="1995"/>
-</tileset>
